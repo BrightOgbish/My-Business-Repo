60762d62b527829071f477e3e8b4f5a7c697cf16
BrightOgbish <brightogbish2016@gmail.com>
2025-08-04 16:08:10 +0100

    Update3 index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to My Business App!');
+  res.send("Welcome to Bright’s Deployed App — my-business-web-app!");
 });
 
 app.listen(port, () => {
